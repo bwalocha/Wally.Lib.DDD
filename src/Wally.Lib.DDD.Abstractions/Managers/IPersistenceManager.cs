57609ac08033f0886163147942274eb7faedf922
wally <b.walocha@gmail.com>
2021-02-27 19:31:26 +0100

    persistence

---
+++ src/Wally.Lib.DDD.Abstractions/Managers/IPersistenceManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wally.Lib.DDD.Abstractions.Managers
+{
+	public interface IPersistenceManager
+	{
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+	}
+}
