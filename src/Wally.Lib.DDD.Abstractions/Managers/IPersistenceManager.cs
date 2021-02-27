bcc6cb65c785f326bf4d1cbf455101cd0a00da28
wally <b.walocha@gmail.com>
2021-02-27 19:51:14 +0100

    GetEntities

--- src/Wally.Lib.DDD.Abstractions/Managers/IPersistenceManager.cs
+++ src/Wally.Lib.DDD.Abstractions/Managers/IPersistenceManager.cs
@@ -1,10 +1,14 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Wally.Lib.DDD.Abstractions.DomainModels;
 
 namespace Wally.Lib.DDD.Abstractions.Managers
 {
 	public interface IPersistenceManager
 	{
 		Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+		IEnumerable<Entity> GetEntities();
 	}
 }
