3701acd390ac3f998ff144ab0abd7c175e5873f0
bwalocha <bwalocha@valuelogic.one>
2021-11-26 02:22:46 +0100

    OData fix

--- src/Wally.Lib.DDD.Abstractions/Queries/PagedQuery.cs
+++ src/Wally.Lib.DDD.Abstractions/Queries/PagedQuery.cs
@@ -3,11 +3,13 @@ using Wally.Lib.DDD.Abstractions.Responses;
 
 namespace Wally.Lib.DDD.Abstractions.Queries
 {
-	public abstract class PagedQuery<TResult> : IQuery<PagedResponse<TResult>> where TResult : IResponse
+	public abstract class PagedQuery<TResult, TOResponse> : IQuery<PagedResponse<TResult>> 
+		where TResult : IResponse
+		where TOResponse : IResponse
 	{
-		public ODataQueryOptions QueryOptions { get; }
+		public ODataQueryOptions<TOResponse> QueryOptions { get; }
 
-		public PagedQuery(ODataQueryOptions queryOptions)
+		public PagedQuery(ODataQueryOptions<TOResponse> queryOptions)
 		{
 			QueryOptions = queryOptions;
 		}
