24e769a99aa36cb30ef5e50ac538cc37274e16fa
wally <b.walocha@gmail.com>
2021-11-27 21:56:32 +0100

    PagedResponse

--- src/Wally.Lib.DDD.Abstractions/Queries/PagedQuery.cs
+++ src/Wally.Lib.DDD.Abstractions/Queries/PagedQuery.cs
@@ -1,10 +1,13 @@
-﻿using Wally.Lib.DDD.Abstractions.Responses;
+﻿using Microsoft.AspNetCore.OData.Query;
+using Wally.Lib.DDD.Abstractions.Requests;
+using Wally.Lib.DDD.Abstractions.Responses;
 
 namespace Wally.Lib.DDD.Abstractions.Queries
 {
 	public abstract class PagedQuery<TResponse> : IQuery<PagedResponse<TResponse>> where TResponse : IResponse
 	{
-		public PagedQuery() : this(new PageInfoQuery())
+		public PagedQuery()
+			: this(new PageInfoQuery())
 		{
 		}
 
@@ -19,5 +22,16 @@ namespace Wally.Lib.DDD.Abstractions.Queries
 		}
 
 		public PageInfoQuery PageInfo { get; }
+	}
+
+	public abstract class PagedQuery<TRequest, TResponse> : IODataQuery<TRequest, PagedResponse<TResponse>>
+		where TRequest : IRequest where TResponse : IResponse
+	{
+		public ODataQueryOptions<TRequest> QueryOptions { get; }
+
+		public PagedQuery(ODataQueryOptions<TRequest> queryOptions)
+		{
+			QueryOptions = queryOptions;
+		}
 	}
 }
